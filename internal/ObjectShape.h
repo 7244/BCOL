8196e4b0f109467d7846d390ce653b513f4810d8
7244 <65ad4d@gmail.com>
2024-01-25 11:33:07 +0000

    up

--- internal/ObjectShape.h
+++ internal/ObjectShape.h
@@ -1,7 +1,4 @@
-void RecycleObject(
-  ObjectID_t ObjectID
-){
-  auto ObjectData = &this->ObjectList[ObjectID];
+void _ClearObject(auto ObjectData){
   for(uint32_t i = 0; i < ObjectData->ShapeList.Current; i++){
     auto ShapeData = &ObjectData->ShapeList.ptr[i];
     switch(ShapeData->ShapeEnum){
@@ -13,8 +10,26 @@ void RecycleObject(
         this->UnlinkRecycleOrphanShape_Rectangle(ShapeData->ShapeID);
         break;
       }
+      case ShapeEnum_t::DPF:{
+        this->UnlinkRecycleOrphanShape_DPF(ShapeData->ShapeID);
+        break;
+      }
     }
   }
+}
+
+void RecycleObject(
+  ObjectID_t ObjectID
+){
+  auto ObjectData = &this->ObjectList[ObjectID];
+  _ClearObject(ObjectData);
   ShapeList_Close(&ObjectData->ShapeList);
   this->ObjectList.Recycle(ObjectID);
+}
+
+/* _ClearObject is internal and doesnt set current to 0 */
+void ClearObject(ObjectID_t ObjectID){
+  auto ObjectData = &this->ObjectList[ObjectID];
+  _ClearObject(ObjectData);
+  ObjectData->ShapeList.Current = 0;
 }
